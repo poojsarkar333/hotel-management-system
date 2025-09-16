38df4657be914114fc199ed980a1f7de76d61062
Pooja <pooja.sarkar333@gmail.com>
2025-09-16 15:37:31 +0530

    Kafka service initial commit

---
+++ notification-service/src/main/java/com/hotel/notification_service/dto/NotificationMessage.java
@@ -0,0 +1,25 @@
+package com.hotel.notification_service.dto;
+
+public class NotificationMessage {
+    private String type;
+    private String message;
+    private Long referenceId;
+
+    // Constructors
+    public NotificationMessage() {}
+
+    public NotificationMessage(String type, String message, Long referenceId) {
+        this.type = type;
+        this.message = message;
+        this.referenceId = referenceId;
+    }
+
+    // Getters & Setters
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+    public Long getReferenceId() { return referenceId; }
+    public void setReferenceId(Long referenceId) { this.referenceId = referenceId; }
+}
+
